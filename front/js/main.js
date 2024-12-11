e1db474ea55c5df6b2981d6953e38350bcb2e548
Mari <mari@MacBook-Pro.local>
2024-12-11 13:41:23 +0200

    no ios fix

--- front/js/main.js
+++ front/js/main.js
@@ -10,3 +10,14 @@
 window.addEventListener("orientationchange", () => {
     location.reload(true)
 });
+
+// padding no-ios
+document.addEventListener("DOMContentLoaded", function() {
+    let bannerDetDate = document.querySelector('.banner__details-date');
+    let bannerDetTime = document.querySelector('.banner__details-time');
+
+    if (!/iPad|iPhone|iPod|Mac/.test(navigator.userAgent)) {
+        bannerDetDate.classList.add('noios-padding');
+        bannerDetTime.classList.add('noios-padding');
+    }
+});
